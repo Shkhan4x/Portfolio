2e3f4fbdfb29a3e29e70e3ffbb3d412505e3b279
plabon khan <123352180+Shkhan4x@users.noreply.github.com>
2024-04-26 08:48:22 +0600

    Add files via upload

---
+++ portfolio/image/love/script.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector('.wrapper')
+const question = document.querySelector('.question')
+const yesBtn = document.querySelector('.yes-btn')
+const noBtn = document.querySelector('.no-btn')
+const wrapperRect = wrapper.getBoundingClientRect();
+console noBtnRect = noBtn.getBoundingClientRect();
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love you too!':
+});
+noBtn.addEventListener('mouseover' ,() -> {})
